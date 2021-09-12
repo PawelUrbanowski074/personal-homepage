68a582533b5efca01b41b3d264248c186203f9ec
palel <32599663+palel@users.noreply.github.com>
2021-09-12 12:06:47 +0200

    main App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import { HeroInfo } from "./components/Hero";
+import Skills from "./components/Skills";
+import { Container } from "./styled";
+import { skills, nextSkills } from "./components/Skills/skills";
 function App() {
   return (
-    <div>
-     hej
-    </div>
+    <Container>
+      <HeroInfo />
+      <main>
+        <Skills
+          header="My skillset includes:"
+          skills={skills}
+        />
+        <Skills
+          header="What I want to lern next:"
+          skills={nextSkills}
+        />
+      </main>
+    </Container>
   );
 }
 
---
+++ src/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1216px;
+    margin: auto;
+    padding-top: 100px;
+    
+`;
