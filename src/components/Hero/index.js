2ae0c056e9390263b19c3055dcf3b9b31c945875
palel <32599663+palel@users.noreply.github.com>
2021-09-12 12:05:54 +0200

    Hero component with style

---
+++ src/components/Hero/index.js
@@ -0,0 +1,20 @@
+import pawel from "../../images/pawel.jpg";
+import { Description, Hero, HireMeButton, HireMeIcon, Image, Name, ThisIs, Tile } from "./styled";
+
+export const HeroInfo = () => (
+  <Tile>
+    <Image src={pawel} alt="" />
+    <Hero>
+      <ThisIs>THIS IS</ThisIs>
+      <Name>Paweł Urbanowski</Name>
+      <Description>
+        Opis moj. asd asd asd asd asfsdv asdf asd awfASD sf WDSFC ASDF wfe ASD wg QFWD wfe WF wge asD agf seg QW
+      </Description>
+      <HireMeButton>
+        <HireMeIcon/>
+        Hire Me
+      </HireMeButton>
+    </Hero>
+  </Tile>
+
+);
