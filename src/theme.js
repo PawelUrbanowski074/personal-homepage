fc779530514c48adee6d1968b9a6e36b553498f2
palel <32599663+palel@users.noreply.github.com>
2021-09-21 23:13:18 +0200

    new color added

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,9 @@ const colorNames = {
   white: "#FFFFFF",
   whiteLilac: "#FBFBFE",
   scienceBlue: "#0366D6",
-  iron: "#D1D5DA",
+  violet2: "#090A3305",
+  violet3: "#090A3308",
+  iron: "#D1D5DA60",
   slateGray: "#6E7E91",
   black: "#000000",
   
@@ -24,5 +26,9 @@ export const themeLight = {
     buttonText: colorNames.white,
     sectionBackground: colorNames.white,
     borderBottom: colorNames.iron,
+    border: colorNames.iron,
+    shadowBorder2: colorNames.violet2,
+    shadowBorder3: colorNames.violet3,
+    blueText: colorNames.scienceBlue,
   },
 };
