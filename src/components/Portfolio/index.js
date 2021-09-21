428d36529cfdf6fda6bbe88fab7c64fcd4b24faa
palel <32599663+palel@users.noreply.github.com>
2021-09-21 23:11:34 +0200

    portfolio component added with styles

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,13 @@
+import GitHubTiles from "./GitHubTiles";
+import Heading from "./Heading";
+import { Section } from "./styled";
+
+const Portfolio = () => (
+  <Section>
+    <Heading />
+    <GitHubTiles />
+  </Section>
+
+);
+
+export default Portfolio;
