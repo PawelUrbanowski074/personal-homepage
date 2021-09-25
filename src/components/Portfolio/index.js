8a5352ed3abf88e898860edfd5f05da20b7617c0
palel <32599663+palel@users.noreply.github.com>
2021-09-25 12:09:59 +0200

    loading and error components added to Portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,13 +1,24 @@
 import GitHubTiles from "./GitHubTiles";
 import Heading from "./Heading";
+import Loading from "./Loading";
+import Error from "./Error";
 import { Section } from "./styled";
 
-const Portfolio = () => (
-  <Section>
-    <Heading />
-    <GitHubTiles />
-  </Section>
+const Portfolio = () => {
 
-);
+  const status = "success";
+  // const status = "loading";
+  // const status = "error";
+
+  return (
+    <Section>
+      <Heading />
+      {status === "success" && <GitHubTiles />}
+      {status === "loading" && <Loading />}
+      {status === "error" && <Error />}
+    </Section>
+
+  );
+};
 
 export default Portfolio;
