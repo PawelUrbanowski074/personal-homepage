b133a877f75f6cdd4016e436494e3ac2e2df9733
palel <32599663+palel@users.noreply.github.com>
2021-09-30 22:31:26 +0200

    theme selector added, personHompage component used

---
+++ src/core/App/index.js
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../../common/themeSlice";
+import { themeLight, themeDark } from "../../theme";
+import { GlobalStyle } from "./globalStyle";
+import PersonalHomepage from "../../features/personalHomepage/PersonalHomepage";
+
+export const App = () => {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+      <GlobalStyle />
+      <PersonalHomepage />
+    </ThemeProvider>
+  );
+}
+
+export default App;
