61aa7f8c6bd7c875aa538417616f79b0232c4547
palel <32599663+palel@users.noreply.github.com>
2021-09-30 22:36:28 +0200

    theme provider added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from "./globalStyle";
-import { themeLight } from "./theme";
-import App from './App';
+import App from './core/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './core/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={themeLight}>
-      < GlobalStyle />
+    <Provider store={store}>
       <App />
-    </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
