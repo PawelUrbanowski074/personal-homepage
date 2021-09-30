bf838d142a37fc8fd6aa32929d1834641b0c50ca
palel <32599663+palel@users.noreply.github.com>
2021-09-30 22:17:32 +0200

    global style file moved to core folder

--- src/core/App/globalStyle.js
+++ src/core/App/globalStyle.js
@@ -13,6 +13,11 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Inter', sans-serif;
     background: ${({ theme }) => theme.color.background};
     color: ${({ theme }) => theme.color.text};
+    transition: background 0.3s;
+    font-size: 18px;
+    letter-spacing: 0.05em;
+    word-wrap: break-word;
+    padding-bottom: 108px;
   }
 
   code {
