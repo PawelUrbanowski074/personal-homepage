df9ee3ea9cc8e302d88654508b34111913b28689
palel <32599663+palel@users.noreply.github.com>
2021-11-22 21:43:46 +0100

    display on mobile device

--- src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Loading/styled.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Loading/styled.js
@@ -5,9 +5,14 @@ export const Wrapper = styled.div`
     text-align: center;
     margin-top: 88px;
     font-size: 20px;
-    color: ${({ theme }) => theme.color.textPrimary };
+    color: ${({ theme }) => theme.color.textPrimary};
     display: grid;
     justify-items: center;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+      font-size: 17px;
+      margin-top: 32px;
+    }
 `;
 
 export const LoadingIcon = styled(Loading)`
@@ -16,13 +21,16 @@ export const LoadingIcon = styled(Loading)`
     animation-duration: 2s;
     animation-iteration-count: infinite;
     animation-timing-function: linear;
-    color: ${({ theme }) => theme.color.spinnerColor };
+    color: ${({ theme }) => theme.color.spinnerColor};
 
     @keyframes rotate {
       to{
         transform: rotate(360deg);
       };
   };
-
+  
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+      margin-top: 32px;
+      width: 60px;
+    }
 `;
-
