761bea7c725a55e536a42323beef82e359fbe746
palel <32599663+palel@users.noreply.github.com>
2021-11-22 21:43:06 +0100

    display on mobile device

--- src/features/personalHomepage/PersonalHomepage/Skills/styled.js
+++ src/features/personalHomepage/PersonalHomepage/Skills/styled.js
@@ -7,6 +7,11 @@ export const Section = styled.section`
   box-shadow: ${({ theme }) => theme.boxShadow};
   border-radius: ${({ theme }) => theme.borderRadiusSmall};
   padding: 32px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px ){
+    margin-top: 48px;
+    padding: 16px;
+  }
 `;
 
 export const Header = styled.h2`
@@ -16,6 +21,11 @@ export const Header = styled.h2`
   padding-bottom: 16px;
   border-bottom: 1px solid ${({ theme }) => theme.color.headerBorderBottom};
   color: ${({ theme }) => theme.color.textPrimary};
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px ){
+    font-size: 18px;
+    padding-bottom: 12px;
+  }
 `;
 
 export const List = styled.ul`
@@ -25,16 +35,30 @@ export const List = styled.ul`
   margin: 32px 0 0 0;
   padding: 0; 
   list-style: none;
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px ){
+    grid-template-columns: 1fr;
+    font-size: 14px;
+    margin-top: 12px;
+  }
 `;
 
 export const Item = styled.li`
-color: ${({ theme }) => theme.color.secondaryText };
-line-height: 140%;
-  /* display: flex; */
+  color: ${({ theme }) => theme.color.secondaryText };
+  line-height: 140%;
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px ){
+    line-height: 120%;
+  }
 `;
 
 export const Bullet = styled(BulletIcon)`
   margin-right: 16px;
   height: auto;
   color: ${({ theme }) => theme.color.boxBullet };
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px ){
+    width: 6px;
+    margin-right: 8px;
+  }
 `;
