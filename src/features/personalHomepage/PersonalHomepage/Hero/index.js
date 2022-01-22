052955c6a056dd4c1403161e55375bd6fe2181b4
palel <32599663+palel@users.noreply.github.com>
2022-01-22 21:12:17 +0100

    photo changed, descriprion completed

--- src/features/personalHomepage/PersonalHomepage/Hero/index.js
+++ src/features/personalHomepage/PersonalHomepage/Hero/index.js
@@ -1,15 +1,16 @@
-import pawel from "./pawel.jpg";
+import pawel2 from "./pawel2.jpg";
 import { StyledButtonLink, Description, HireMeIcon, Image, Name, ThisIs, Wrapper } from "./styled";
 import { email } from "../email";
 
 const HeroInfo = () => (
   <Wrapper>
-    <Image src={pawel} alt="Pawel Urbanowski" />
+    <Image src={pawel2} alt="Pawel Urbanowski" />
     <div>
       <ThisIs>This Is</ThisIs>
       <Name>Paweł Urbanowski</Name>
       <Description>
-        I’m a passionate Frontend Developer in love with&nbsp;React,currently looking for new job opportunities.
+        I’m a passionate Frontend Developer in love with&nbsp;React,currently looking for new job opportunities. <br/>
+        I would like to constantly expand my skills, improve my knowledge so that the tasks entrusted to me are professionally carried out within the prescribed time, and the reviewed code is more pleasing to the eye.
       </Description>
       <StyledButtonLink href={`mailto:${email}`} title={email}>
         <HireMeIcon/>
